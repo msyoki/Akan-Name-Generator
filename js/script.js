e228e34dbb2a57a39c97a3766d131ef0dfb56d7f
Msyoki <msyokimutua@gmail.com>
2020-07-17 20:50:15 +0300

    added test if statement

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-const date =  document.getElementById("date")
-const month =  document.getElementById("month")
-const year =  document.getElementById("year")
-const gender =  document.getElementById("gender")
-const form =  document.getElementById("form")
+const date = parseInt(document.getElementById("date").value);
+const month = parseInt(document.getElementById("month").value);
+const year = parseInt(document.getElementById("year").value);
+const gender = parseInt(document.getElementById("gender").value);
+const form = parseInt(document.getElementById("form").value);
+
+if (date <1 || date >31){
+    document.getElementsByClassName(*dayerror).innerHtml ="Enter valid date";
+    return false;
+}else {
+    d.setDate(date);
+}
