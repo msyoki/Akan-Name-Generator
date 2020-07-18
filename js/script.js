77feeb5def73c273735b8dc715cd7f0af2db863f
Msyoki <msyokimutua@gmail.com>
2020-07-18 17:11:32 +0300

    merge

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const date = parseInt(document.getElementById("date").value);
-const month = parseInt(document.getElementById("month").value);
-const year = parseInt(document.getElementById("year").value);
-const gender = parseInt(document.getElementById("gender").value);
-const form = parseInt(document.getElementById("form").value);
-
-if (date <1 || date >31){
-    document.getElementsByClassName(dayerror).innerHtml ="Enter valid date";
-    return false;
-}else {
-    d.setDate(date);
-}
-=======
 var daysOfTheWeek=["Monday","Tuesday","Wednesday","Thursday","Friday"];
 var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
@@ -22,7 +8,3 @@ function days() {
    var y = document.getElementById("year").value
 }
 
-Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-
-+1
->>>>>>> test
