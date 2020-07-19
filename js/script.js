14d8e0985cf6253192bf3b71840ed3c0da5f70d6
Msyoki <msyokimutua@gmail.com>
2020-07-19 08:55:11 +0300

    added  form reset function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-var daysOfTheWeek=["Monday","Tuesday","Wednesday","Thursday","Friday"];
-var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-
-function days() {
-   var d = document.getElementById("date").value
-   var m = document.getElementById("month").value
-   var y = document.getElementById("year").value
+ 
+function formRefresh(){
+   document.getElementById("form_data").reset();
 }
 
+
+
+/*
+   var daysWeek=["Monday","Tuesday","Wednesday","Thursday","Friday"];
+   var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+   var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
