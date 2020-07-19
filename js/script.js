c69aa05dad3bc20d32e30e064053429e3aa1ae76
Msyoki <msyokimutua@gmail.com>
2020-07-19 08:59:16 +0300

    added form variables to akan name finder function,century calculation and day of the week as per profided formula

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ function formRefresh(){
 
 
 
+function akanNames(){
+   var day=parseInt(document.getElementById("day").value);
+   var month=parseInt(document.getElementById("month").value);
+   var year=parseInt(document.getElementById("year").value);
+
+   //Centuary calculation
+   var centry=(year-1)/100+1; 
+   var dayOfTheWeek = ( centry/4 -2*centry-1 + 5*year/4  + 26*(month+1)/10 + day ) % 7;
+
 /*
    var daysWeek=["Monday","Tuesday","Wednesday","Thursday","Friday"];
    var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
