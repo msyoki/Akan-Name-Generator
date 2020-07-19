14dd439227a8644750503826aa1e94f2214b17a5
Msyoki <msyokimutua@gmail.com>
2020-07-19 09:04:38 +0300

    added more variables and a display function based on gender  and day of the wee k calculation

--- js/script.js
+++ js/script.js
@@ -14,6 +14,13 @@ function akanNames(){
    var centry=(year-1)/100+1; 
    var dayOfTheWeek = ( centry/4 -2*centry-1 + 5*year/4  + 26*(month+1)/10 + day ) % 7;
 
+   document.getElementById("display").innerHTML=Math.round(dayOfTheWeek);
+    var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var maleNames=["Kwasi"," Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var gender=document.getElementById("gender").value;
+    var daysOfWeek=["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var dOfWeek = Math.round(dayOfTheWeek)
+
 /*
    var daysWeek=["Monday","Tuesday","Wednesday","Thursday","Friday"];
    var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
